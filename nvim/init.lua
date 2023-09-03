78c14495c22f33db4fc49cfc9ffccb239474f3c4
HanniWalter <hannibali@gmx.de>
2023-09-03 18:09:26 +0200

    mergeconflict

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,7 @@
 require "user.options"
 require "user.keymaps"
 require "user.plugins"
+require "user.cmp"
+
 --require "user.cheatsheet"
 vim.cmd "colorscheme codedark" 
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -39,9 +39,20 @@ return packer.startup(function(use)
   --use "Mofiqul/vscode.nvim"
   use 'tomasiser/vim-code-dark' 
   
-  use "hrsh7th/nvim-cmp"
-  use "HanniWalter/nvim-cheatsheet"
-  -- use "/home/johannes/Projects/cheatsheet.nvim/"
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions"
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+
+
+  --  use "HanniWalter/nvim-cheatsheet"
+  use "/home/johannes/Projects/cheatsheet.nvim/"
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
    
